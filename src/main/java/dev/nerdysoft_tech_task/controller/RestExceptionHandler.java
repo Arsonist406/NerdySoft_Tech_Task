90730ef34c5c96188fc03341c0fbfbc0bf7296ab
Ostap <ostap06seniv@gmail.com>
2025-06-07 22:57:23 +0300

    feat: rewrite RestExceptionHandler
    - remove inner ErrorDto
    - unify handling of BusinessException subclasses
    - add handling of MethodArgumentNotValidException, ConstraintViolationException, NotUniqueException, BookCantBeBorrowed

--- src/main/java/dev/nerdysoft_tech_task/controller/RestExceptionHandler.java
+++ src/main/java/dev/nerdysoft_tech_task/controller/RestExceptionHandler.java
@@ -1,57 +1,92 @@
 package dev.nerdysoft_tech_task.controller;
 
-import dev.nerdysoft_tech_task.exception.CantBeDeletedException;
-import dev.nerdysoft_tech_task.exception.NotFoundException;
-import lombok.Builder;
+import dev.nerdysoft_tech_task.dto.ErrorDTO;
+import dev.nerdysoft_tech_task.exception.*;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @Builder
-    public record ErrorDto(
-            LocalDateTime timestamp,
-            int status,
-            String error,
-            String message,
-            String path
-    ) {}
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ErrorDto notFound(
-            NotFoundException e,
-            WebRequest request
+    private ErrorDTO buildErrorDTO(
+            WebRequest request,
+            Set<ErrorDTO.ErrorDetail> details
     ) {
-        return ErrorDto
+        return ErrorDTO
                 .builder()
                 .timestamp(LocalDateTime.now())
-                .status(HttpStatus.NOT_FOUND.value())
-                .error(HttpStatus.NOT_FOUND.name())
-                .message(e.getMessage())
-                .path(request.getDescription(false))
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.name())
+                .path(request.getDescription(false).replace("uri=", ""))
+                .details(details)
                 .build();
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(CantBeDeletedException.class)
-    public ErrorDto cantBeDeleted(
-            CantBeDeletedException e,
+    @ExceptionHandler({
+            BookCantBeBorrowedException.class,
+            CantBeDeletedException.class,
+            NotFoundException.class,
+            NotUniqueException.class
+    })
+    public ErrorDTO businessException(
+            BusinessException e,
             WebRequest request
     ) {
-        return ErrorDto
+        Set<ErrorDTO.ErrorDetail> details = Set.of(
+                ErrorDTO.ErrorDetail
                 .builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.NOT_FOUND.value())
-                .error(HttpStatus.NOT_FOUND.name())
+                .value(null)
                 .message(e.getMessage())
-                .path(request.getDescription(false))
-                .build();
+                .build());
+
+        return buildErrorDTO(request, details);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ErrorDTO constraintViolation(
+            ConstraintViolationException e,
+            WebRequest request
+    ) {
+        Set<ErrorDTO.ErrorDetail> details = e.getConstraintViolations()
+                .stream()
+                .map(violation ->
+                        ErrorDTO.ErrorDetail
+                                .builder()
+                                .value(violation.getPropertyPath().toString())
+                                .message(violation.getMessage())
+                                .build())
+                .collect(Collectors.toSet());
+
+        return buildErrorDTO(request, details);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorDTO methodArgumentValidation(
+            MethodArgumentNotValidException e,
+            WebRequest request
+    ) {
+        Set<ErrorDTO.ErrorDetail> details = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError ->
+                        ErrorDTO.ErrorDetail
+                                .builder()
+                                .value(fieldError.getField())
+                                .message(fieldError.getDefaultMessage())
+                                .build())
+                .collect(Collectors.toSet());
+
+        return buildErrorDTO(request, details);
     }
 }
