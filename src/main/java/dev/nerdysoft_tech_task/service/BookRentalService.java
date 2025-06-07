87f4f86d84084f325fc2f9c4e7b1cee39e86c85c
Ostap <ostap06seniv@gmail.com>
2025-06-08 00:33:25 +0300

    feat: remove redundant BookRental classes chain

--- src/main/java/dev/nerdysoft_tech_task/service/BookRentalService.java
+++
@@ -1,8 +0,0 @@
-package dev.nerdysoft_tech_task.service;
-
-public interface BookRentalService {
-
-    void borrowBook(Long memberId, Long bookId);
-
-    void returnBook(Long memberId, Long bookId);
-}
